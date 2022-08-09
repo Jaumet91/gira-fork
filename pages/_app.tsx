6bd96ee4d6565f2a38d2b00d96fc7d1d7374ce1e
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-09 16:00:45 +0200

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import '../styles/globals.css';
+import { lightTheme, darkTheme } from '../themes';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+const HomePage: NextPage = () => {
+  return (
+    <Typography variant="h1" color="primary">
+      Hola mundo
+    </Typography>
+  );
+};
+
+export default HomePage;
