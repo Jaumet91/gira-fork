a8d80c1a5b08c48e102a4041c024ff1a042bdbab
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-09 16:20:55 +0200

    Create Layout and Navbar

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,21 @@
+import { FC, PropsWithChildren } from 'react';
+import Head from 'next/head';
+
+import { Box } from '@mui/system';
+import { Navabar } from '../ui';
+
+interface LayoutProps extends PropsWithChildren<{}> {
+  title?: string;
+}
+
+export const Layout: FC<LayoutProps> = ({ title = 'OpenJira', children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Navabar />
+      <Box sx={{ padding: '10px 20px' }}>{children}</Box>
+    </Box>
+  );
+};
