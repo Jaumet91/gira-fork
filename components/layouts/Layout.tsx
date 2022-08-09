0aeba4f25fe397877501594d41f84a656aaac121
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-09 16:50:36 +0200

    Implement Sidebar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 import Head from 'next/head';
 
 import { Box } from '@mui/system';
-import { Navabar } from '../ui';
+import { Navabar, Sidebar } from '../ui';
 
 interface LayoutProps extends PropsWithChildren<{}> {
   title?: string;
@@ -15,6 +15,7 @@ export const Layout: FC<LayoutProps> = ({ title = 'OpenJira', children }) => {
         <title>{title}</title>
       </Head>
       <Navabar />
+      <Sidebar />
       <Box sx={{ padding: '10px 20px' }}>{children}</Box>
     </Box>
   );
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,48 @@
+import {
+  Drawer,
+  Box,
+  Typography,
+  List,
+  ListItemIcon,
+  ListItemButton,
+  ListItemText,
+  Divider,
+} from '@mui/material';
+
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+
+const menuItems: string[] = ['Inbox', 'Starred', 'Send Email', 'Drafts'];
+
+export const Sidebar = () => {
+  return (
+    <Drawer anchor="left" open={true} onClose={() => console.log('cerrando')}>
+      <Box sx={{ width: 250 }}>
+        <Box sx={{ padding: '5px 10px' }}>
+          <Typography variant="h4">Menu</Typography>
+        </Box>
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItemButton key={text}>
+              <ListItemIcon>
+                {index % 2 ? <InboxOutlinedIcon /> : <EmailOutlinedIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {menuItems.map((text, index) => (
+            <ListItemButton key={text}>
+              <ListItemIcon>
+                {index % 2 ? <InboxOutlinedIcon /> : <EmailOutlinedIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
