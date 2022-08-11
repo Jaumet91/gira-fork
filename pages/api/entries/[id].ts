48490e63204aa2eb7f9b9d2a2b2439bc750e10b4
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-11 10:44:43 +0200

    Finish update endpoint

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -40,14 +40,24 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     status = entryToUpdate.status,
   } = req.body;
 
-  const updatedEntry = await Entry.findByIdAndUpdate(
-    id,
-    {
-      description,
-      status,
-    },
-    { runValidators: true, new: true },
-  );
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      {
+        description,
+        status,
+      },
+      { runValidators: true, new: true },
+    );
+    await db.disconnect();
+    res.status(200).json(updatedEntry!);
+  } catch (error: any) {
+    console.log({ error });
+    await db.disconnect();
+    res.status(400).json({ message: error.errors.status.message });
+  }
 
-  res.status(200).json(updatedEntry!);
+  // entryToUpdate.description = description;
+  // entryToUpdate.status = status;
+  // await entryToUpdate.save();
 };
